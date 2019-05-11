c388c58cb8ea2946ac9082f2216f7cf0e343369f
s17001 <s17001@std.it-college.ac.jp>
2019-05-11 17:17:53 +0900

    firestoreからデータ取ってこれるようにした

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
+import firebase from "firebase";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import firebase from "firebase";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: function(h) {
-    return h(App);
-  }
-}).$mount("#app");
 
 // Your web app's Firebase configuration
 let firebaseConfig = {
@@ -25,3 +16,12 @@ let firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: function(h) {
+    return h(App);
+  }
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,52 @@
   <div class="home">
     <h1>俺たちがチャットルム一覧だ！！！！</h1>
     <input type="button" value="新しくチャットルームを作ろうと思うんだ" />
+  
+  
+  <ul v-for="(room,i) in rooms" :key="'room-id-'+i">
+    <li>
+      <p>{{room.name}}</p>
+      <p>{{room.num}}</p>
+      <input type="button" value="じょいんじょいんとき" >
+    </li>
+  </ul>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import firebase, { firestore } from 'firebase';
 
 export default {
   name: "home",
   components: {
     HelloWorld
-  }
+  },
+  
+  created() {
+    firebase
+    .firestore()
+    .collection("rooms")
+    .get()
+    .then(ss => {
+      const rooms = [];
+
+      ss.docs.forEach(room => {
+        rooms.push( room.data() )
+      })
+
+      this.rooms = rooms
+    });
+  },
+
+  data () {
+    return {
+      rooms:[],
+    }
+  },
 };
+//この書き方するとめちゃくちゃ実行速度が遅くなるけど…
 </script>
+
