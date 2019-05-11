f05803cdafc322c4082501b767a7eba66025a745
s17001 <s17001@std.it-college.ac.jp>
2019-05-11 17:44:22 +0900

    removed

--- src/main.js
+++ src/main.js
@@ -5,13 +5,7 @@ import router from "./router";
 
 // Your web app's Firebase configuration
 let firebaseConfig = {
-  apiKey: "AIzaSyAMoqdHG3K2icUaXNNZ7CjjQ4RY9KA00dk",
-  authDomain: "vue-chat-ee7b2.firebaseapp.com",
-  databaseURL: "https://vue-chat-ee7b2.firebaseio.com",
-  projectId: "vue-chat-ee7b2",
-  storageBucket: "vue-chat-ee7b2.appspot.com",
-  messagingSenderId: "216082941162",
-  appId: "1:216082941162:web:c9061e02cafc13d6"
+
 };
 
 // Initialize Firebase
